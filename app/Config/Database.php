f8044b78d531dfd4368516c66b18fbd9585163e6
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-10-14 10:45:51 +0100

    Databse config.

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,9 +32,9 @@ class Database extends \CodeIgniter\Database\Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => '',
-		'password' => '',
-		'database' => '',
+		'username' => 'root',
+		'password' => '13759806',
+		'database' => 'cafevariomenet',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
