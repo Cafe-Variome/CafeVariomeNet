6c8b784593aac43ff38bab52a7bc5f4b84f40128
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-10-14 18:03:32 +0100

    Added KeyGen class.

---
+++ app/Libraries/Net/KeyGen.php
@@ -0,0 +1,24 @@
+<?php namespace App\Libraries\Net;
+
+/**
+ * KeyGen.php
+ * 
+ * Created: 14/10/2019
+ * 
+ * @author Mehdi Mehtarizadeh
+ */
+
+
+class KeyGen 
+{
+
+    public function generateInstallationKey()
+    {
+        return md5($this->generateMD5());
+    }
+
+    private function generateMD5() {
+        $mdstring = md5(uniqid(rand(), true));
+        return $mdstring;
+    }
+}
