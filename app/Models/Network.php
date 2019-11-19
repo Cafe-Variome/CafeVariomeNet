4ff461e912ae8c9eaaa9b1d03fcdb641b5a3933f
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-11-19 15:03:25 +0000

    Compatibility with NetworkAPIResponse.

--- app/Models/Network.php
+++ app/Models/Network.php
@@ -16,12 +16,14 @@ use CodeIgniter\Database\ConnectionInterface;
  class Network extends Model
  {
     protected $table      = 'networks';
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'network_key';
 
     protected $returnType = 'array';
 
     protected $db;
     protected $builder;
+
+    public $response;
 
     public function __construct(ConnectionInterface &$db = null){
         if ($db != null) {
@@ -31,6 +33,32 @@ use CodeIgniter\Database\ConnectionInterface;
             $this->db = \Config\Database::connect();
         }
         $this->builder = $this->db->table($this->table);
+
+    }
+
+    private function initiateResponse(int $status, array $data = null)
+    {
+        $this->response = new NetworkAPIResponse($status, $data);
+    }
+
+    private function setResponseMessage(string $message)
+    {
+        $this->response->setMessage($message);
+    }
+
+    public function getResponse(): NetworkAPIResponse
+    {
+        return $this->response;
+    }
+
+    public function getResponseArray(): array
+    {
+        return $this->response->toArray();
+    }
+
+    public function getResponseJSON(): string
+    {
+        return $this->response->toJSON();
     }
 
     public function getNetworks(string $cols = null, array $conds = null, array $groupby = null, bool $isDistinct = false, int $limit = -1, int $offset = -1){
@@ -58,13 +86,26 @@ use CodeIgniter\Database\ConnectionInterface;
         return $query; 
     }
 
-    public function createNetwork(array $data): int
+    public function createNetwork(array $data, bool $uniquename = false): int
     {
         try {
+            if ($uniquename) {
+                $networks = $this->getNetworks('network_name', ['network_name' => $data['network_name']]);
+                if (count($networks) > 0){
+                    //network name is not unique
+                    $this->initiateResponse(0);
+                    $this->setResponseMessage('Network name is not unique.');
+                    return -1;
+                }
+            }
             $this->builder->insert($data);
-            return $this->db->insertID();
+            $network_key =  (int)$this->db->insertID();
+            $this->initiateResponse(1, ['network_key' => $network_key]);
+            return $network_key;
         } catch (\Exception $ex) {
             error_log($ex->getMessage());
+            $this->initiateResponse(0);
+            $this->setResponseMessage($ex->getMessage());
             return -1;
         }
     }
@@ -74,9 +115,12 @@ use CodeIgniter\Database\ConnectionInterface;
         try {
             $this->builder = $this->db->table('installations_networks');
             $this->builder->insert(['installation_key' => $installation_key, 'network_key' => $network_key]);
+            $this->initiateResponse(1);
             return true;
         } catch (\Exception $ex) {
             error_log($ex->getMessage());
+            $this->initiateResponse(0);
+            $this->setResponseMessage($ex->getMessage());
             return false;
         }
         return false;
