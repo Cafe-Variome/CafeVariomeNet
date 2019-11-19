87ca9c8efc311c4f3b718f709461a3342278f23a
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-11-19 15:00:53 +0000

    createNetwork added.
    Along with addInstallationToNetwork.
    
    Typo in constructor name fixed.

--- app/Controllers/NetworkApi.php
+++ app/Controllers/NetworkApi.php
@@ -23,12 +23,11 @@ use App\Models\Network;
 
 class NetworkApi extends ResourceController{
 
-    public function initCotroller(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    private $networkModel;
+
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         parent::initController($request, $response, $logger);
-
-
-
     }
 
     public function validateInstallation()
@@ -46,6 +45,36 @@ class NetworkApi extends ResourceController{
         return $this->respond($networkModel->getNetworks());
     }
 
+    public function createNetwork()
+    {
+        $network_name = $this->request->getVar("network_name");
+        $network_type = $this->request->getVar("network_type");
+        $network_threashold = $this->request->getVar("network_threshold");
+        $network_status = $this->request->getVar("network_status");
+		$data = ['network_name' => $network_name,
+				 'network_type' => $network_type,
+				 'network_threshold' => $network_threashold,
+				 'network_status' => $network_status
 
+                ]; 
+
+        $networkModel = new Network();
+        
+        $networkModel->createNetwork($data, true);
+
+        return $this->respond($networkModel->getResponseJSON());      
+    }
+
+    public function addInstallationToNetwork()
+    {
+        $installation_key = $this->request->getVar("installation_key");
+        $network_key = $this->request->getVar("network_key");
+
+        $networkModel = new Network();
+
+        $networkModel->addInstallationToNetwork($installation_key, $network_key);
+        
+        return $this->respond($networkModel->getResponseJSON());      
+    }
 
 }
