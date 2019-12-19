07565a3976d2623649c5160b68ddc299624cb79d
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-12-19 17:11:27 +0000

    getResponseCode method added.
    Along with major bug fixes.

--- app/Libraries/Net/NetworkInterface.php
+++ app/Libraries/Net/NetworkInterface.php
@@ -22,18 +22,39 @@ class NetworkInterface
 
     private $networkAdapter;
 
-    public function __construct(string $uri) {
+    private $responseCode;
 
-        $this->targetURI = $uri;
+    public function __construct(string $uri = null)
+    {
+        if ($uri) {
+            $this->targetURI = $uri;
+        }
         $curlOptions = [CURLOPT_RETURNTRANSFER => TRUE];
         $this->networkAdapter = new cURLAdapter(null, $curlOptions);
     }
 
+    public function setTargetURI(string $uri)
+    {
+        $this->targetURI = $uri;
+    }
+
     public function RequestToJoinNetwork(int $network_key, string $installation_key, string $email, string $justification, string $ipAddress, string $token, string $url)
     {
-        $this->adapterw('networkapi/requestToJoinNetwork', ['network_key' => $network_key, 'email' => $email, 'justification' => $justification]);
+        $this->adapterw('networkapi/requestToJoinNetwork', ['network_key' => $network_key, 'installation_key' => $installation_key, 'email' => $email, 'justification' => $justification, 'ip_address' => $ipAddress, 'token' => $token, 'url' => $url]);
         $response = $this->networkAdapter->Send();
+        $this->responseCode = $this->networkAdapter->getHTTPResponseCode();
         return $this->processResponse($response);
+    }
+
+    public function getResponseCode(): int
+    {
+        return $this->responseCode;
+    }
+
+    private function processResponse($response)
+    {
+        $responseObj = json_decode($response);
+        return $responseObj;
     }
 
     public function adapterw(string $uriTail, array $data)
